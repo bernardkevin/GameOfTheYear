4205e50aabd917e4fcb1a9f11972b69dbc3560ad
Kevin Bernard <bernardkevin025@gmail.com>
2015-10-28 12:38:03 +0100

    Planete 2d + player

---
+++ Assets/GravitySphere2D/GravityBody2d.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityBody2d : MonoBehaviour {
+
+	//public GameObject Planet;
+	public GravityAttractor2d attractor;
+	public Transform myTransform;
+
+	void Start () {
+		//attractor = Planet.GetComponent<GravityAttractor>();
+		GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+		//GetComponent<Rigidbody2D>().useGravity = false;
+		myTransform = transform;
+	}
+	
+	void Update () {
+		attractor.Attract(myTransform);
+	}
+}
---
+++ Assets/GravitySphere2D/PlayerController2d.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController2d : MonoBehaviour {
+	public float moveSpeed = 15;
+	private Vector3 moveDir;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveDir = new Vector2(Input.GetAxisRaw("Horizontal"),0).normalized;
+	}
+
+	void FixedUpdate(){
+		GetComponent<Rigidbody2D>().MovePosition(GetComponent<Rigidbody2D>().position + (Vector2)transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
+	}
+}
